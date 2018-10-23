db65bc9f58b680d6eef5aadf82fb33789eb4b71d
Elizabeth Hernandez <deliaeliza72@gmail.com>
2018-10-22 20:15:25 -0600

    push metodos menos, arbolbin

--- ElMundoDeLosAnimales/ElMundoDeLosAnimales/arbolbin.h
+++ ElMundoDeLosAnimales/ElMundoDeLosAnimales/arbolbin.h
@@ -20,102 +20,11 @@ typedef struct nodo* NODOPTR;
 //#############################################################
 //                PROTOTIPOS DE FUNCIÓN                      ##   
 //#############################################################
-static int profundidad(NODOPTR);
-static void recorridoPreOrden(NODOPTR);
-static void recorridoEnOrden(NODOPTR);
-static void recorridoPosOrden(NODOPTR);
-static void levelOrderTraversalRecursivo(NODOPTR);
-static void levelOrder(NODOPTR, int);
-static void levelOrderTraversalIterativo(NODOPTR);
 static void borrarArbol(NODOPTR);
 static NODOPTR buscarNodo(NODOPTR, std::string);
 static void imprimeArbol(NODOPTR);
 static void print(NODOPTR, std::string, bool);
 //#############################################################
-
-///////////////////////////////////////////////////////////////////////
-///<summary>Calcula la profundidad (altura) del árbol. </summary>
-///<returns>Retorna el valor de la profundidad (Depth) </returns>
-int profundidad(NODOPTR actual) {
-	if (actual == nullptr)
-		return 0;
-	int maxizq = profundidad(actual->left);
-	int maxder = profundidad(actual->right);
-
-	if (maxizq > maxder)
-		return maxizq + 1;
-	return maxder + 1;
-}
-
-///////////////////////////////////////////////////////////////////////
-///<summary> Imprime el recorrido del árbol Pre Orden </summary>
-void recorridoPreOrden(NODOPTR actual) {
-	if (actual != nullptr) {
-		std::cout << actual->elemento << " ";
-		recorridoPreOrden(actual->left);
-		recorridoPreOrden(actual->right);
-	}
-}
-
-///////////////////////////////////////////////////////////////////////
-///<summary> Imprime el recorrido del árbol En Orden </summary>
-void recorridoEnOrden(NODOPTR actual) {
-	if (actual != nullptr) {
-		recorridoPreOrden(actual->left);
-		std::cout << actual->elemento << " ";
-		recorridoPreOrden(actual->right);
-	}
-}
-
-///////////////////////////////////////////////////////////////////////
-///<summary> Imprime el recorrido del árbol Pos Orden </summary>
-void recorridoPosOrden(NODOPTR actual) {
-	if (actual != nullptr) {
-		recorridoPreOrden(actual->left);
-		recorridoPreOrden(actual->right);
-		std::cout << actual->elemento << " ";
-	}
-}
-
-///////////////////////////////////////////////////////////////////////
-///<summary> WRAPPER - Imprime el recorrido del árbol en Level-Order (Breadth-first search) RECURSIVO </summary>
-void levelOrderTraversalRecursivo(NODOPTR actual) {
-	int niveles = profundidad(actual) + 1;
-	for (int i = 1; i <= niveles; ++i)
-		levelOrder(actual, i);
-}
-
-///////////////////////////////////////////////////////////////////////
-/// <summary> Auxiliar para el recorrido del árbol en Level-Order </summary>
-void levelOrder(NODOPTR actual, int n) {
-	if (n == 1)
-		std::cout << actual->elemento << " ";
-	else {
-		levelOrder(actual->left, n - 1);
-		levelOrder(actual->right, n - 1);
-	}
-}
-
-///////////////////////////////////////////////////////////////////////
-/// <summary> Imprime el recorrido del árbol en Level-Order (Breadth-first search) ITERATIVO </summary>
-void levelOrderTraversalIterativo(NODOPTR actual) {
-	std::queue<NODOPTR> Cola;
-	NODOPTR tmp;
-
-	if (!actual)
-		return;
-
-	Cola.push(actual);
-	while (!Cola.empty()) {
-		tmp = Cola.front();
-		Cola.pop();
-		std::cout << tmp->elemento << " ";
-		if (tmp->left)
-			Cola.push(tmp->left);
-		if (tmp->right)
-			Cola.push(tmp->right);
-	}
-}
 
 ///////////////////////////////////////////////////////////////////////
 /// <summary> Elimina el árbol completo, nodo por nodo. Utiliza un recorrido Pos Orden para tal fin. </summary>
@@ -151,6 +60,12 @@ NODOPTR buscarNodo(NODOPTR actual, std::string dato) {
 ///<summary></summary>
 ///<returns></returns>
 void imprimeArbol(NODOPTR p){
+	std::string guion, signo;
+	guion = (char)196;
+	signo = (char)195;
+	std::cout << "\t\t\t " << static_cast<char>(192) + guion + guion + guion + " Root\n";
+	std::cout << "\t\t\t\t" << signo + guion + guion + guion + " Right\n";
+	std::cout << "\t\t\t\t" << static_cast<char>(192) + guion + guion + guion + " Left\n\n";
 	print(p, " ", true);
 }
 
